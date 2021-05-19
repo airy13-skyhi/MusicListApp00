6f2f337ef7a8193e5e4227c38c5ff466aef6f09b
github airy13-skyhi <airy13@icloud.com>
2021-05-19 12:10:09 +0900

    Initial Commit

---
+++ MusicListApp00/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicListApp00
+//
+//  Created by Manabu Kuramochi on 2021/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
